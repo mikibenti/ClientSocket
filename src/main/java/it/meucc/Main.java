6b518b342223807d69a102937475fc4fd9a33a40
FabioZavataro <fabiozavataro@gmail.com>
2024-10-09 09:13:49 +0200

    finito

--- src/main/java/it/meucc/Main.java
+++ src/main/java/it/meucc/Main.java
@@ -1,12 +1,36 @@
 package it.meucc;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws UnknownHostException, IOException {
-        System.out.println("Client partito");
-        Socket mySocket = new Socket("localhost", 5000);
+    public static void main(String[] args) {
+        try {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("INSERISCI LA STRINGA : ");
+            String outputString = sc.nextLine();
+            sc.close();
+            System.out.println("Client partito");
+            Socket mySocket;
+            mySocket = new Socket("localhost", 4000);
+            System.out.println("CLIENT COLLEGATO");
+            BufferedReader in = new BufferedReader(new InputStreamReader(mySocket.getInputStream()));
+            DataOutputStream out = new DataOutputStream(mySocket.getOutputStream());
+            out.writeBytes(outputString + '\n');
+            String stringRed = in.readLine();
+            System.out.println("Stringa ricevuta : " + stringRed);
+            mySocket.close();
+        } catch (UnknownHostException e) {
+            System.out.println("ERRORE");
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.out.println("ERRORE");
+            e.printStackTrace();
+        }
     }
 }
